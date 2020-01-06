54089d037c3126c6dc2b87c784123fd61f11d0bc
yuriygumeniuk <yuriygumeniuk@gmail.com>
2020-01-06 02:40:25 +0200

    sliders finish

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,17 @@
 $(document).ready(function () {
 
-function ibg(){
-    $.each($('.ibg'), function(index, val) {
-        if($(this).find('.img_bg').length>0){
-            $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
-            $(this).find('.img_bg').css('display','none');
-        }
-    });
-}
-ibg();
-  // slider banner
-  var mySwiper = new Swiper('.swiper-container', {
+// function ibg(){
+//     $.each($('.ibg'), function(index, val) {
+//         if($(this).find('.img_bg').length>0){
+//             $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+//             $(this).find('.img_bg').css('display','none');
+//         }
+//     });
+// }
+// ibg();
+
+// slider-banner
+  var mySwiper = new Swiper('.slider-banner', {
       // roundLengths: true,
       slidesPerView: 1,
       speed: 2000,
@@ -19,29 +20,66 @@ ibg();
         nextEl: '.forward',
         prevEl: '.backward',
       },
+  }); 
+// var mySwiper = new Swiper('.slider-banner');
+//   $('.forward').on('click', function(){
+//       mySwiper.navigation.nextEl;
+//   });
+//   $('.backward').on('click', function(){
+//       mySwiper.navigation.prevEl;
+//   });
+// slider-banner end
+
+//////////////////////////////////
+//////////////////////////////////
+
+// slider-recent
+  var recentSwiper = new Swiper('.slider-recent', {
+    slidesPerView: 4,
+    // roundLengths: true,
+    spaceBetween: 19,
+    freeMode: false,
+    speed: 1500,
+    navigation: {
+        nextEl: '#recent-forward',
+        prevEl: '#recent-backward',
+    },
+    loop: true,
   });
-  
-var mySwiper = new Swiper('.swiper-container');
-  $('.forward').on('click', function(){
-      mySwiper.navigation.nextEl;
-  });
-  $('.backward').on('click', function(){
-      mySwiper.navigation.prevEl;
-  });
-  // slider new + featured
-  // var mySwiper = new Swiper('.new-product-block', {
-  //   slidesPerView: 4,    
-  //   slidesPerGroup: 2,
-  //   loop: true,    
-  //   spaceBetween: 8,
-  //   freeMode: false,
-  //   speed: 4000,
-  //   autoplay: {
-  //     delay: 5000,
-  //     disableOnInteraction: false,
-  //   },
+  // var recentSwiper = new Swiper('.slider-recent');
+  // $('#recent-forward').on('click', function(){
+  //     recentSwiper.navigation.nextEl;
   // });
-  
+  // $('#recent-backward').on('click', function(){
+  //     recentSwiper.navigation.prevEl;
+  // });
+
+
+// slider-recent end
+
+
+// var featuredSwiper = new Swiper('.swiper-container');
+//   $('.forward').on('click', function(){
+//       mySwiper.navigation.nextEl;
+//   });
+//   $('.backward').on('click', function(){
+//       mySwiper.navigation.prevEl;
+//   });
+
+// slider-featured
+  var featuredSwiper = new Swiper('.slider-featured', {
+    loop: true,    
+    slidesPerView: 4,
+    spaceBetween: 19,
+    freeMode: false,
+    speed: 1500,
+    navigation: {
+        nextEl: '#featured-forward',
+        prevEl: '#featured-backward',
+    },
+  });
+
+
     // var featuredSwiper = document.querySelector('#featured-slider').swiper;
     //     $('#featured-slider').hover(function(){
     //          featuredSwiper.autoplay.stop();
