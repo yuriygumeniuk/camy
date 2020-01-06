0cc7d510e809788b66fcb69db778499cdc20a0e7
yuriygumeniuk <yuriygumeniuk@gmail.com>
2020-01-06 22:42:40 +0200

    btn go to top

--- js/custom.js
+++ js/custom.js
@@ -6,15 +6,31 @@ $(document).ready(function () {
     return false
   });
 
-// function ibg(){
-//     $.each($('.ibg'), function(index, val) {
-//         if($(this).find('.img_bg').length>0){
-//             $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
-//             $(this).find('.img_bg').css('display','none');
-//         }
-//     });
-// }
-// ibg();
+// scroll to top
+    // $('body').append('<a href="#" id="go-top" title="Вверх"></a>')
+    $(function() {
+        $.fn.scrollToTop = function() {
+            $(this).hide().removeAttr("href");
+            if ($(window).scrollTop() >= "500")
+                $(this).fadeIn("slow")
+            
+            var scrollDiv = $(this)
+            $(window).scroll(function() {
+                if ($(window).scrollTop() <= "500")
+                    $(scrollDiv).fadeOut("slow")
+                else $(scrollDiv).fadeIn("slow")
+            })
+            
+            $(this).click(function() {
+                $("html, body").animate({scrollTop: 0}, "slow")
+            })
+        }   
+    })   
+
+    $(function() {
+        $("#go-top").scrollToTop()
+        console.log(window.innerHeight)
+    })
 
 // slider-banner
   var mySwiper = new Swiper('.slider-banner', {
